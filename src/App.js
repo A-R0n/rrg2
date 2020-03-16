3792b8c5d1774f9847d7de7aa5c9dd5637284ce6
Aaron Estes <aestesc@gmail.com>
2020-03-15 21:59:58 -0500

    Login functionality using useReducer

--- src/App.js
+++ src/App.js
@@ -1,26 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
+import Login from './Login.js';
 import './App.css';
+// this is a pure function so outside the component
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+
+      <Login />
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ src/Login.js
@@ -0,0 +1,111 @@
+import React, {useReducer} from 'react';
+import './Login.css';
+import {login} from './utils';
+
+function loginReducer(state, action) {
+    switch (action.type) {
+        case 'field': {
+            return {
+                ...state,
+                [action.field]: action.value
+
+            }
+        }
+        case 'login': {
+            return {
+                ...state,
+                isLoading: true,
+                error: ''
+            };
+        }
+        case 'success': {
+            return {
+                ...state,
+                isLoggedIn: true
+            }
+        }
+        case 'error': {
+            return {
+                ...state,
+                error: 'incorrect username or password',
+                isLoading: false,
+                username: '',
+                password: ''
+            }
+        }
+        case 'logout': {
+            return {
+                ...state,
+                isLoggedIn: false,
+                isLoading: false,
+                username: '',
+                password: ''
+            }
+        }
+    
+        default:
+            break;
+    }
+    return state;
+}
+
+const initialState = {
+    username: '',
+    password: '',
+    isLoading: false,
+    error: '',
+    isLoggedIn: false
+};
+
+export default function Login(){
+    const [state, dispatch] = useReducer(loginReducer, initialState);
+
+    const {username, password, isLoading, error, isLoggedIn} = state;
+    const onSubmit = async e => {
+        e.preventDefault();
+
+        dispatch({type: 'login'});
+
+        try {
+            await login({ username, password });
+            dispatch({type: 'success'});
+        } catch(error){
+            dispatch({type: 'error'})
+        }
+    };
+
+    return (
+        <div className="Login">
+            <div className="login-container">
+                {isLoggedIn ? (
+                 <>
+                    <div className="greetings">Greetings {username}</div>{' '}
+                    <button onClick={()=>dispatch({type: 'logout'})}>Log out</button>
+                </>
+                ) :
+                <form className='loginForm' onSubmit={onSubmit}>
+                    <p className="pleaseLogin">Please Login!</p>
+                    <input
+                        id='username'
+                        type="text"
+                        placeholder="username"
+                        value={username}
+                        onChange={e => dispatch({type: 'field', field: 'username', value: e.currentTarget.value})}></input>
+                    <input 
+                        id='password'
+                        type="password"
+                        placeholder="password"
+                        autocomplete="new-password"
+                        value={password}
+                        onChange={e => dispatch({type: 'field', field: 'password', value: e.currentTarget.value})}>
+                    </input>
+                    {error && <p className="error">{error}</p>}
+                    <button id="loginButton" type="submit" disabled={isLoading}>
+                        {isLoading ? "Logging in..." : "Log in"}
+                    </button>
+                </form>}
+            </div>
+        </div>
+    )
+
+}
