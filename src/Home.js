524ad7d33641f21365f0f701572f4dc24e0a413f
A-R0n <aestesc@gmail.com>
2020-03-21 11:22:04 -0500

    Redirected user to login after succesffully signing out

--- src/Home.js
+++ src/Home.js
@@ -2,10 +2,15 @@ import React from 'react';
 import app from './base';
 
 const Home = () => {
+    console.log(app.name);
     return(
         <div>
             <h1>Home</h1>
-            <button onClick={()=> app.auth().signOut()}>Sign Out</button>
+            <button onClick={()=> app.auth().signOut().then(()=>{
+                window.location = "/login";
+            }).catch((error)=>{
+                console.log(`an error: ${error} - happened signing user out`);
+            })}>Sign Out</button>
         </div>
     );
 };
